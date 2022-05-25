7186488b29e98e87e93afe8d6d67289bc7afe34f
Jan VZ <jan.vz94@gmail.com>
2022-05-25 14:16:57 +0200

    Initial Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,20 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+#
+require 'csv'
+
+CSV.foreach(Rails.root.join('lib/seed_csv/flights.csv'), headers: true) do |row|
+  Flight.create( {
+                   number: row['number'],
+                  departureAirport: row['departure_airport'],
+                   arrivalAirport: row['arrival_airport'],
+                  businessPlaces: row['business_class_seats'].to_i,
+                  ecoPlaces: row['economy_class_seats'].to_i,
+                   date: row['departure_date'],
+                  duration: row['duration'].to_i,
+                } )
+end
+
